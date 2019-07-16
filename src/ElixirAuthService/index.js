1d89c229794962b18e364140fdcc10106cdef8dc
Trish Whetzel <plwhetzel@gmail.com>
2019-07-16 13:12:14 +0100

    add current updates

--- src/ElixirAuthService/index.js
+++ src/ElixirAuthService/index.js
@@ -11,19 +11,93 @@ export default class ElixirAuthService {
         //     isAuthenticated: false
         // };
 
+        this._domain = encodeURIComponent(window.location.origin);
+        this._appURL = process.env.REACT_APP_AAPURL.replace(/\/$/, '');
+
         this.getProfile = this.getProfile.bind(this);
 
         //This binding is necessary to make `this` work in the callback
         // this.handleLogin = this.handleLogin.bind(this);
+        this.login = this.login.bind(this);
+
     }
 
     // handleLogin = () => {
     login = () => {
         // Example: https://gitlab.ebi.ac.uk/tools-glue/ng-ebi-authorization/blob/master/src/auth/auth.service.ts
-        console.log("** Elixir Button clicked!")
-        window.open("https://api.aai.ebi.ac.uk/sso?from=http%3A%2F%2Flocalhost%3A3000", "_blank");
+        // console.log("** Elixir Button clicked!")
 
+        var width = 650;
+        var height = 1000;
+        var left = -1;
+        var top = -1;
+
+        if (left < 0) {
+            const screenWidth = window.screen.width;
+            if (screenWidth > width) {
+                left = Math.round((screenWidth - width) / 2);
+            }
+        }
+        if (top < 0) {
+            const screenHeight = window.screen.height;
+            if (screenHeight > height) {
+                top = Math.round((screenHeight - height) / 2);
+            }
+        }
+
+        const windowOptions = [
+            `width=${width}`,
+            `height=${height}`,
+            `left=${left}`,
+            `top=${top}`,
+            'personalbar=no',
+            'toolbar=no',
+            'scrollbars=yes',
+            'resizable=yes',
+            'directories=no',
+            'location=no',
+            'menubar=no',
+            'titlebar=no',
+            'toolbar=no'
+        ];
+
+        const loginWindow = window.open(this.getSSOURL(), 'Sign in to Elixir', windowOptions.join(','));
+
+        if (loginWindow) {
+            loginWindow.focus();
+        }
     }
+
+
+    /**
+    * Produces a URL that allows logging into the single sign on (SSO) page.
+    * The URL is opened in a new window using window.open().
+    *
+    * @returns The SSO URL.
+    */
+    getSSOURL() {
+        const fragments = this._formatFragments({
+            'from': this._domain,
+        });
+        return `${this._appURL}/sso${fragments}`;
+    }
+
+
+    /**
+    * Format and filter fragment options
+    *
+    * @params options
+    *
+    * @returns fragment string
+    */
+    _formatFragments(options) {
+        return '?' + Object.entries(options)
+            .map(([key, value]) => `${key}=${value}`).join('&');
+    }
+
+
+
+
 
     // componentDidMount() {
     //     window.addEventListener('message', (event) => {
@@ -54,10 +128,12 @@ export default class ElixirAuthService {
         try {
             const decoded = jwt_decode(token);
             if (decoded.exp < Date.now() / 1000) { // Checking if token is expired.
+                console.log("** Token is EXPIRED!");
                 return true;
             }
             else
-                return false;
+                console.log("** Token is NOT EXPIRED!");
+            return false;
         }
         catch (err) {
             return false;
@@ -82,17 +158,6 @@ export default class ElixirAuthService {
     getProfile() {
         // Using jwt-decode npm package to decode the token
         return jwt_decode(this.getToken());
-    }
-
-
-
-    componentWillUnmount() {
-        window.removeEventListener('logout', this.logout);
-        console.log("** Removed listener");
-
-        // Clear state... not sure if needed
-        // this.setState({ isLoggedIn: false });
-        // console.log("** ElixirAuth Login State: ", this.state.isAuthenticated);
     }
 
 
